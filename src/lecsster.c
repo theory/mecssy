b16331ff4a823e8b22e4fd1e97a7ef613bcdc7a6
David E. Wheeler <david@justatheory.com>
2013-03-07 20:54:52 -0800

    Actually do the parse.
    Seeing a parse error, but passes anyway.

--- src/lecsster.c
+++ src/lecsster.c
@@ -11,7 +11,25 @@ int parse_css(FILE *fh) {
     // Set up the scanner
     yyscan_t scanner;
     yylex_init(&scanner);
-//    yyset_in(fh, scanner);
+    yyset_in(fh, scanner);
+
+    // Set up the parser
+    void *css_parser = ParseAlloc(malloc);
+    int lexCode;
+
+    do {
+        lexCode = yylex(scanner);
+        Parse(css_parser, lexCode, yyget_text(scanner));
+    } while (lexCode > 0);
+
+    if (-1 == lexCode) {
+        fprintf(stderr, "The scanner encountered an error.\n");
+        return 0;
+    }
+
+    // Cleanup the scanner and parser
+    yylex_destroy(scanner);
+    ParseFree(css_parser, free);
 
     return 1;
 
